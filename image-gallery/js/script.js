7bebb4e60bc69944ffc1800a0b6bd89285cc71f2
ViacheslavAnisimov <forvsn@gmail.com>
2023-09-26 11:33:13 +0700

    feat: add close button script

--- image-gallery/js/script.js
+++ image-gallery/js/script.js
@@ -1,5 +1,7 @@
 const mainContainer = document.querySelector('.main-container');
 const headerSearchInput = document.querySelector('.header-search');
+const headerCloseButton = document.querySelector('.header-close-btn');
+const searchForm = document.querySelector('.search-form');
 
 searchForm.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -8,11 +10,16 @@ searchForm.addEventListener('submit', (event) => {
   getData(searchWord);
 });
 
+headerCloseButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  headerSearchInput.value = '';
+});
+
 async function getData(search) {
-  const response = await fetch(`https://api.unsplash.com/search/photos?query=${search}&per_page=30&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo`)
+  const response = await fetch(`https://api.unsplash.com/search/photos?query=${search}&per_page=30&orientation=landscape&client_id=p3UTBU5SZsaooCpbmGKjkUtXC8aMbPNZJOUbTElAJOI`)
   const data = await response.json();
   imgGrid(data);
-}
+};
 
 function imgGrid(data) {
   for (let imgJson of data.results) {
@@ -20,5 +27,7 @@ function imgGrid(data) {
     img.setAttribute('src', imgJson.urls.small);
     img.classList.add('img-item');
     mainContainer.appendChild(img);
-  }
-}
+  };
+};
+
+window.onload = () => getData('Mountains');
