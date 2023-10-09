77326998a447dd094bf91afea63a3cd2e94ef741
ViacheslavAnisimov <forvsn@gmail.com>
2023-10-09 10:42:04 +0700

    feat: create grid

---
+++ random-game/js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid');
+  let width = 10;
+  let squares = [];
+
+  //create Board
+  function createBoard() {
+    for(let i = 0; i < width * width; i++) {
+      const square = document.createElement('div');
+      square.setAttribute('id', i);
+      grid.appendChild(square);
+      squares.push(square);
+    }
+  }
+  createBoard();
+});
