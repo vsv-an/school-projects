2e43311dc2bb102e2ae4f98af9adf985884b3b45
ViacheslavAnisimov <forvsn@gmail.com>
2023-10-09 11:36:31 +0700

    feat: add bombs to grid

--- random-game/js/script.js
+++ random-game/js/script.js
@@ -1,15 +1,51 @@
 document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid');
   let width = 10;
+  let bombAmount = 20;
   let squares = [];
 
   //create Board
   function createBoard() {
+    //get shuffled game array with random bombs
+    const bombsArray = Array(bombAmount).fill('bomb');
+    const emptyArray = Array(width * width - bombAmount).fill('valid');
+    console.log(bombsArray);
+    console.log(emptyArray);
+    const gameArray = emptyArray.concat(bombsArray);
+    console.log(gameArray);
+    const shuffledArray = gameArray.sort(() => Math.random() - 0.5);
+    console.log(shuffledArray);
+
     for(let i = 0; i < width * width; i++) {
       const square = document.createElement('div');
       square.setAttribute('id', i);
+      square.classList.add(shuffledArray[i]);
       grid.appendChild(square);
       squares.push(square);
+    }
+
+    //normal click
+    square.addEventListener('click', function(e) {
+      click(square);
+    })
+
+    //add numbers
+    for(let i = 0; i < squares.length; i++) {
+      let total = 0;
+      const isLeftEdge = (i % width === 0);
+      const isRightEdge = (i % width === width - 1);
+
+      if (squares[i].classList.contains('valid')) {
+        if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains('bomb')) total++;
+        if (i > 9 && !isRightEdge && squares[i + 1 - width].classList.contains('bomb')) total++;
+        if (i > 10 && squares[i - width].classList.contains('bomb')) total++;
+        if (i > 11 && !isLeftEdge && squares[i - 1 - width].classList.contains('bomb')) total++;
+        if (i < 98 && !isRightEdge && squares[i + 1].classList.contains('bomb')) total++;
+        if (i < 90 && !isLeftEdge && squares[i - 1 + width].classList.contains('bomb')) total++;
+        if (i < 88 && !isRightEdge && squares[i + 1 + width].classList.contains('bomb')) total++;
+        if (i < 89 && squares[i + width].classList.contains('bomb')) total++;
+        squares[i].setAttribute('data', total);
+      }
     }
   }
   createBoard();
